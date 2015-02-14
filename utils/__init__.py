8cb9731b477fd1dd285d1a3bd8659465517c3353
wraith-wireless <wraith.wireless@hushmail.com>
2015-02-14 08:33:26 -0700

    initial version v 0.0.1

---
+++ utils/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+""" utils: holds utility functions """
+__name__ = 'utils'
+__license__ = 'GPL'
+__version__ = '0.0.1'
+__date__ = 'February 2015'
+__author__ = 'Dale Patterson'
+__maintainer__ = 'Dale Patterson'
+__email__ = 'wraith.wireless@hushmail.com'
+__status__ = 'Production'
