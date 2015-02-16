c1af8006ecef637f73f7805d5c5494ea7070d7f5
wraith-wireless <wraith.wireless@hushmail.com>
2015-02-16 08:46:46 -0700

    initial version v 0.0.1

--- __init__.py
+++ __init__.py
@@ -13,6 +13,13 @@ Requires:
  PIL (python-pil,python-imaging-tk,tk8.5-dev tcl8.5-dev) NOTE: I had to unistall
   python-pil and reinstall after installing tk8.5 and tcl8.5
 
+wraith 0.0.2
+ desc: dyskt,nidus are developmentally sound, begin work on gui
+ includes: wraith-rt.py and wraith.conf (also all subdirectories etc)
+ changes:
+  - added extraction of all management frames (excluding timing adv)
+  - added nidusd daemon file to start nidus server
+
  TODO:
   1) tried --remove-pid/--remove-pidfile to remove pids of dysktd and nidusd
      from /var/run but does not work - figure out how to delete the pidfile
@@ -21,7 +28,7 @@ Requires:
 __name__ = 'wraith'
 __license__ = 'GPL'
 __version__ = '0.0.2'
-__date__ = 'December 2014'
+__date__ = 'February 2015'
 __author__ = 'Dale Patterson'
 __maintainer__ = 'Dale Patterson'
 __email__ = 'wraith.wireless@hushmail.com'
